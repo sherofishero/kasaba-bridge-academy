8bdc76a3d44b5aca93f5d843cb77ba160f973a61
Kasabalilar <kasabalilar@local.com>
2026-07-15 16:59:51 +0300

    Add communication layer abstraction

---
+++ app/lib/communication.ts
@@ -0,0 +1,11 @@
+import { TableState, TablePlayer } from "./game";
+
+export type TableEventHandler = (state: TableState) => void;
+
+export interface TableCommunication {
+  createTable(tableId: string, initialState: TableState): Promise<TableState>;
+  joinTable(tableId: string, player: TablePlayer): Promise<TableState>;
+  leaveTable(tableId: string, player: TablePlayer): Promise<TableState>;
+  updateTableState(tableId: string, state: TableState): Promise<TableState>;
+  subscribeToTable(tableId: string, handler: TableEventHandler): () => void;
+}
